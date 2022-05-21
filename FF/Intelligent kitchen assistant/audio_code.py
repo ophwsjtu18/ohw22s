8afef1c1cc689dc69818b7835a4f26ba976a1b45
catanduni <102228205+catanduni@users.noreply.github.com>
2022-05-21 08:11:13 +0800

    Create audio_code.py

---
+++ FF/Intelligent kitchen assistant/audio_code.py
@@ -0,0 +1,14 @@
+from aip import AipSpeech
+import os
+
+APP_ID = '25496064'
+API_KEY = "A6fXM6nA1B8GY2txDIUCXYyu"
+SECRET_KEY = "4qb3jX1C8ue1rhwMkp27kzmrxLTli9G8" 
+
+
+client=AipSpeech(APP_ID,API_KEY,SECRET_KEY)
+
+voice=client.synthesis("麻辣烫",'zh',6,{'vol':15,'per':3,'spd':5})
+with open("audio.wav",'wb') as fp:
+    fp.write(voice)
+os.system("audio.wav")
